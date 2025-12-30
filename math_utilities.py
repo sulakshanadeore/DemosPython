0d260cb59978de0e5cdb36ca19c6f4c5d721b913
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-12-30 12:46:44 +0530

    Modules folder

---
+++ math_utilities.py
@@ -0,0 +1,16 @@
+class UserMaths:
+
+    def __init__(self):
+        pass 
+
+    def add(self,a,b):
+        return a+b
+
+    def divide(self,a,b):
+     if b>0:
+       return a/b
+    
+    def subtract(self,a,b):
+        if(b>0):
+           return a-b
+    
